09060b0204f43e5c647019db6be99fc72ceddb1e
mertmcd <merttdem@gmail.com>
2025-02-14 14:02:00 +0300

    feat: add Babel configuration and response interceptor for handling 401 errors; update dependencies in package.json

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -20,6 +20,19 @@ axiosInstance.interceptors.request.use(
   }
 );
 
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.replace("/login");
+    }
+    return Promise.reject(new Error(error.message));
+  }
+);
+
 export const fetchProducts = async (currentPage: number, limit: number) => {
   const response = await axiosInstance.get(
     `/products?limit=${limit}&skip=${(currentPage - 1) * limit}`
